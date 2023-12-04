e7a4c8e539900b44c9c396dbc6d8bac633a48910
Marleen Jattke <Marleen.Jattke@bsi-software.com>
2023-12-04 15:10:05 +0100

    Language switch fixes and extension

--- templates/website/includes/navigation/languageSwitch.js
+++ templates/website/includes/navigation/languageSwitch.js
@@ -1,23 +1,52 @@
-import Alpine from 'alpinejs';
+document.addEventListener('alpine:init', () => {
+    window.Alpine.data('languageSwitch', () => ({
+        rootEl: null,
+        activeLanguageElement: null,
+        availableLanguageElements: null,
+        availableLanguages: null,
 
-Alpine.data('languageSwitch', () => ({
-    format(language) {
-        return language.slice(0,2).toUpperCase();
-    },
-    init() {
-        let activeLanguage = this.$root.querySelector('.active-language');
-        let languages = this.$root.querySelectorAll('.select-language');
-        if (!activeLanguage || languages.length <= 1) {
-            this.$root.classList.add('d-none'); // hide language panel
-        } else {
-            activeLanguage.innerText = this.format(activeLanguage.innerText);
-            languages.forEach(lang => {
-                lang.innerText = this.format(lang.innerText);
-                if (lang.innerText == activeLanguage.innerText) {
-                    lang.classList.add('current-active');
-                }
-            });
+        init() {
+            this.rootEl = this.$root;
+            this.activeLanguageElement = this.$root.querySelector('.active-language');
+            this.availableLanguageElements = this.$root.querySelectorAll('.available-language');
+
+            if (!this.activeLanguageElement || this.availableLanguageElements.length <= 1) {
+                this.rootEl.classList.add('d-none'); // hide language panel
+            } else {
+                this.availableLanguages = {};
+                this.availableLanguageElements.forEach(el => {
+                    let key = this._formatLanguage(el.innerText);
+
+                    // check if language already exists (e.g. 'de' in case the two locales de-CH and de-DE exist), if so change key to locale
+                    if (this.availableLanguages[key]) {
+                        this.availableLanguages[el.innerText] = el.innerText;
+                        let locale = this.availableLanguages[key];
+                        this.availableLanguages[locale] = locale;
+                        delete this.availableLanguages[key];
+                    } else {
+                        this.availableLanguages[key] = el.innerText;
+                    }
+                });
+
+                this.availableLanguageElements.forEach(el => {
+                    let language = null;
+                    for (let key in this.availableLanguages) {
+                        if (el.innerText === this.availableLanguages[key]) {
+                            el.innerText = key;
+                            language = this.availableLanguages[key];
+                        }
+                    }
+                    el.setAttribute('aria-label', "Switch to language " + el.innerText);
+                    if (language === this.activeLanguageElement.innerText) {
+                        el.classList.add('current-active');
+                        this.activeLanguageElement.innerText = el.innerText;
+                    }
+                });
+            }
+        },
+
+        _formatLanguage(language) {
+            return language.slice(0,2).toUpperCase();
         }
-
-    }
-}));
+    }))
+})
--- templates/website/modules/main.js
+++ templates/website/modules/main.js
@@ -1,4 +1,3 @@
 import '../includes/navigation/languageSwitch';
 
 import '@bsi-cx/design-standard-library-web/core/main/index';
-
