6ab3188b6c30a73b7ac3f340c558b374703f00b6
Lukas Steiger <lukas.steiger@bsi-software.com>
2024-01-31 17:29:16 +0100

    Improved translations (#371869, #368259)

--- properties-de.js
+++ properties-de.js
@@ -15,6 +15,24 @@ module.exports = {
   /* ------------------------- Select ---------------------------- */
   selectOptionText: 'Bitte wählen',
 
+  /* ------------------------- Slot finder ----------------------- */
+  slotFinderChooseDateText: 'Datum wählen',
+  slotFinderChooseSlotText: 'Termin wählen',
+  slotFinderBookSlotTex:'Termin überprüfen',
+  slotFinderBookSlotButtonText: 'Termin buchen',
+  slotFinderNoSlotsText: 'Keine Termine verfügbar',
+  slotFinderChooseAnotherDayText: 'Bitte anderen Tag wählen',
+
+  /* ------------------------- Webcam ---------------------------- */
+	webcamImgModeAskTitleText: 'Kamerazugriff benötigt',
+	webcamImgModeAskDescriptionText: 'Wir benötigen Zugriff auf ihre Kamera, um ein Bild aufzunehmen. Falls ihr Browser Sie im nächsten Schritt um die Berechtigung zur Nutzung der Kamera fragt, muss der Zugriff auf die Kamera gewährt werden.',
+	webcamImgModeAskButtonCameraText: 'Zugriff erlauben',
+	webcamImgModeAskButtonManualText: 'Stattdessen ein Bild hochladen',
+	webcamImgModeCameSubmitButtonText: 'Bild übermitteln',
+	webcamImgModeManualTitleText: 'Bild hochladen',
+	webcamImgModeManualDescriptionText: 'Kein Zugriff auf die Kamera gewünscht oder möglich. Laden Sie stattdessen ein Bild hoch.',
+	webcamImgModeManualUploadImageButtonText: 'Bild hochladen',
+
   /* ----------------------- Error Texts ------------------------- */
   errorRequiredText: 'Bitte füllen Sie dieses Feld aus.',
   errorInvalidText: 'Der Wert, den Sie für dieses Feld eingegeben haben, ist ungültig.'
--- properties-en.js
+++ properties-en.js
@@ -15,6 +15,24 @@ module.exports = {
   /* ------------------------- Select ---------------------------- */
   selectOptionText: 'Please select',
 
+  /* ------------------------- Slot finder ----------------------- */
+  slotFinderChooseDateText: 'Choose date',
+  slotFinderChooseSlotText: 'Choose time slot',
+  slotFinderBookSlotTex:'Check time slot',
+  slotFinderBookSlotButtonText: 'Book time slot',
+  slotFinderNoSlotsText: 'No slots available',
+  slotFinderChooseAnotherDayText: 'Please choose another day',
+
+  /* ------------------------- Webcam ---------------------------- */
+	webcamImgModeAskTitleText: 'Camera access required',
+	webcamImgModeAskDescriptionText: 'We need access to your camera to take a picture. If your browser asks you for authorisation to use the camera in the next step, access to the camera must be granted.',
+	webcamImgModeAskButtonCameraText: 'Allow access',
+	webcamImgModeAskButtonManualText: 'Upload a picture instead',
+	webcamImgModeCameSubmitButtonText: 'Submit picture',
+	webcamImgModeManualTitleText: 'Upload picture',
+	webcamImgModeManualDescriptionText: 'No access to the camera desired or possible. Upload a picture instead.',
+	webcamImgModeManualUploadImageButtonText: 'Upload picture',
+
   /* ----------------------- Error Texts ------------------------- */
   errorRequiredText: 'Please fill out this field.',
   errorInvalidText: 'The value you entered for this field is invalid.'
