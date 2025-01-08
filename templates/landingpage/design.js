acf4820ea0d4b0063012a76e2f73662c323c34ef
Lukas Heyer <lukas.heyer@bsi-software.com>
2025-01-08 11:59:05 +0100

    Removed list and list-item

--- templates/landingpage/design.js
+++ templates/landingpage/design.js
@@ -67,9 +67,7 @@ module.exports = cx.design
         require('@bsi-cx/design-standard-library-web/content-elements/form/select'),
         require('@bsi-cx/design-standard-library-web/content-elements/form/checkbox'),
         require('@bsi-cx/design-standard-library-web/content-elements/form/radio'),
-        require('@bsi-cx/design-standard-library-web/content-elements/form/poll'),
-        require('@bsi-cx/design-standard-library-web/content-elements/form/list'),
-        require('@bsi-cx/design-standard-library-web/content-elements/form/list-item')),
+        require('@bsi-cx/design-standard-library-web/content-elements/form/poll')),
     cx.contentElementGroup
       .withGroupId('footer-lJ2X2V')
       .withLabel('Footer')
