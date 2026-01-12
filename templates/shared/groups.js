fdef5599a6b2442deacd6523c9b40f1dc55f2c1b
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-12 09:39:21 +0100

    add button group

--- templates/shared/groups.js
+++ templates/shared/groups.js
@@ -9,12 +9,14 @@
 const { oldContentElementGroup } = require('@bsi-cx/design-standard-library-web/content-elements/_old-content-elements');
 
 const { layoutGroup } = require('@bsi-cx/design-standard-library-web/content-elements/layout');
+const { buttonGroup } = require('@bsi-cx/design-standard-library-web/content-elements/buttons');
 
 sharedGroups = [
     // TODO: Kann später raus, ist nur zum prüfen ob in der StandardLib alles korrekt referenziert ist
     oldContentElementGroup,
 
     layoutGroup,
+    buttonGroup,
     
 ]
 
