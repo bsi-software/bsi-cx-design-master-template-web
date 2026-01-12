fc3ddfe0f353aa6e61322c31263ee05c5c8abd5c
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-12 09:39:33 +0100

    add new properties feature (working)

--- properties.js
+++ properties.js
@@ -1,18 +1,18 @@
 const properties = require('@bsi-cx/design-standard-library-web/styles/properties.js');
 
-const { css } = require('@bsi-cx/design-build');
+// const { css } = require('@bsi-cx/design-build');
 
-const primaryColor = css.color('#fe9915');
-const secondaryColor = css.color('#0082a1');
-const tertiaryColor = css.color('#2bb198');
-const darkColor = css.color('#333333');
-const lightColor = css.color('#ffffff');
-const infoColor = css.color('#abb3b8');
+// const primaryColor = css.color('#fe9915');
+// const secondaryColor = css.color('#0082a1');
+// const tertiaryColor = css.color('#2bb198');
+// const darkColor = css.color('#333333');
+// const lightColor = css.color('#ffffff');
+// const infoColor = css.color('#abb3b8');
 
-const invalidColor = css.color('#d11928');
-const validColor = css.color('#19804b');
+// const invalidColor = css.color('#d11928');
+// const validColor = css.color('#19804b');
 
-const textColor = css.color('#282322');
+// const textColor = css.color('#282322');
 
 module.exports = {
   ...properties,
@@ -21,16 +21,16 @@ module.exports = {
   /*	                           Colors	                           */
   /* ============================================================= */
 
-  // override colors from standardLib
-  primaryColor,
-  secondaryColor,
-  tertiaryColor,
-  darkColor,
-  lightColor,
-  infoColor,
-  invalidColor,
-  validColor,
-  textColor,
+  // // override colors from standardLib
+  // primaryColor,
+  // secondaryColor,
+  // tertiaryColor,
+  // darkColor,
+  // lightColor,
+  // infoColor,
+  // invalidColor,
+  // validColor,
+  // textColor,
 
   /* ============================================================= */
   /*	                           Fonts	                           */
@@ -44,25 +44,25 @@ module.exports = {
   /*	                      Grid breakpoint	                       */
   /* ============================================================= */
 
-  gridBreakpointMobile: '375px', // 375px-767px
-  gridBreakpointTablet: '768px', // 768px-1024px
-  gridBreakpointDesktop: '1025px', // 1025px +
+  // gridBreakpointMobile: '375px', // 375px-767px
+  // gridBreakpointTablet: '768px', // 768px-1024px
+  // gridBreakpointDesktop: '1025px', // 1025px +
 
   /* ============================================================= */
   /*	                      Layout Styling	                       */
   /* ============================================================= */
 
-  // this ist the standard gap for the bootstrap css grid
-  gridGap: '24px',
-  // this is the gap between every Element in the content.
-  // If you set it to 0 there is no gap because Elements has no margin.
-  gapBetweenElements: '24px',
-  // this is the gap between elements inside of an column
-  gapBetweenElementsInColumns: '8px',
-  // this ist the padding of each column
-  colPadding: '16px',
-  // border-radius for column element
-  columnRoundetCorner: '8px',
+  // // this ist the standard gap for the bootstrap css grid
+  // gridGap: '24px',
+  // // this is the gap between every Element in the content.
+  // // If you set it to 0 there is no gap because Elements has no margin.
+  // gapBetweenElements: '24px',
+  // // this is the gap between elements inside of an column
+  // gapBetweenElementsInColumns: '8px',
+  // // this ist the padding of each column
+  // colPadding: '16px',
+  // // border-radius for column element
+  // columnRoundetCorner: '8px',
 
 
   /* ============================================================= */
