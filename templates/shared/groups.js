006e7e32efa19c6fe670f0ef0241e6e71b0fca4e
kno-bsi <keri.nogarth@bsi-software.com>
2026-01-13 09:37:33 +0100

    add text element group

--- templates/shared/groups.js
+++ templates/shared/groups.js
@@ -10,6 +10,7 @@ const { oldContentElementGroup } = require('@bsi-cx/design-standard-library-web/
 
 const { layoutGroup } = require('@bsi-cx/design-standard-library-web/content-elements/layout');
 const { buttonGroup } = require('@bsi-cx/design-standard-library-web/content-elements/buttons');
+const { textGroup } = require('@bsi-cx/design-standard-library-web/content-elements/text-elements');
 
 sharedGroups = [
     // TODO: Kann später raus, ist nur zum prüfen ob in der StandardLib alles korrekt referenziert ist
@@ -17,7 +18,7 @@ sharedGroups = [
 
     layoutGroup,
     buttonGroup,
-    
+    textGroup,
 ]
 
 module.exports.landingpageGroups = sharedGroups
