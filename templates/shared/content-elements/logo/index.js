39aa54b7e7fa1943719d1c8ba2bfac3a48c82682
mima-bsi <Miriana.Martini@bsi-software.com>
2026-02-12 11:32:34 +0100

    fix: MR comments

--- templates/shared/content-elements/logo/index.js
+++ templates/shared/content-elements/logo/index.js
@@ -6,9 +6,4 @@ module.exports = contentElement(
   require('./template.twig'),
   'logo-pm7eOT',
   'Logo',
-  'logo-lm-part-image-1rKWxD',
-  'Dark- & Light-Mode',
-  'logo-dm-part-image-ySZsrw',
-  'Logo Light mode',
-  'Logo Dark mode'
 ).withStyleConfigs();
