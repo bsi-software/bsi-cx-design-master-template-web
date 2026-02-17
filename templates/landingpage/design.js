ba0588bb03653e0c6f9d78719d493ca167d07909
kno-bsi <keri.nogarth@bsi-software.com>
2026-02-17 11:30:51 +0100

    WIP

--- templates/landingpage/design.js
+++ templates/landingpage/design.js
@@ -9,7 +9,7 @@ module.exports = cx.design
   .withAuthor('Design Team - BSI Business Systems Integration AG')
   .withDate(new Date().toLocaleString("de-DE", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit" }))
   .withPreviewImage('static/header.png')
-  // TODO: build function for automatic generation of this line
+  // TODO: build function for automatic generation of this line -> is verry comlicated, we decide to don't do it yet
   .withHtmlEditorConfigs(require("@bsi-cx/design-standard-library-web/content-elements/text-elements/body-text/editor-config.js"))
   .withContentElementGroups(
     ...require('../shared/groups').landingpageGroups,
